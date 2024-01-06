20265a64a1e6891fb05a403384a336e74ad38cb5
BLUEBEAST\katze <katzenberger.ph+github@gmail.com>
2024-01-06 21:15:25 +0100

    feat: base og image config

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
   <NuxtPage />
 </template>
+
+<script lang="ts" setup>
+useHead({
+  titleTemplate: "%s | App-Name",
+  meta: [{ name: "description", content: "My amazing site." }],
+});
+defineOgImageComponent("NuxtSeo", {
+  colorMode: "dark",
+});
+</script>
