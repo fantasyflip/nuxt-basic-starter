8e8f82a7559ea46a33e28c5d9519aa45eed3e1cb
BLUEBEAST\katze <katzenberger.ph+github@gmail.com>
2024-01-06 22:55:09 +0100

    feat(config): initialize color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   // NUXT-OG-IMAGE: .env -> NUXT_PUBLIC_SITE_URL
-  modules: ["@nuxtjs/tailwindcss", "nuxt-og-image", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-og-image",
+    [
+      "@nuxtjs/color-mode",
+      {
+        preference: "system", // default value of $colorMode.preference
+        fallback: "dark", // fallback value if not system preference found
+        classSuffix: "",
+        classPrefix: "",
+        storageKey: "theme",
+      },
+    ],
+  ],
 });
