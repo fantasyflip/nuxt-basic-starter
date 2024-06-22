24cf069330c4854cb01169e91b98103ed254cca4
FantasyFlip <>
2024-06-22 10:22:41 +0200

    feat: add k8s scripts

---
+++ scripts/k8s-init.js
@@ -0,0 +1,226 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+
+// Function to create directory if it doesn't exist
+function createDirectory(dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+}
+
+// Function to read values from package.json
+function getK8sConfig() {
+  const packageJsonPath = path.join(__dirname, "..", "package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  return packageJson.k8s || {};
+}
+
+// Function to update package.json with k8s config
+function updateK8sConfig(namespace, serviceName, appName, infraRepo) {
+  const packageJsonPath = path.join(__dirname, "..", "package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  packageJson.k8s = { namespace, serviceName, appName, infraRepo };
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(packageJson, null, 2),
+    "utf8",
+  );
+}
+
+// Function to prompt the user for input
+function prompt(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise((resolve) =>
+    rl.question(question, (ans) => {
+      rl.close();
+      resolve(ans);
+    }),
+  );
+}
+
+// Main function to generate files
+async function main() {
+  let k8sConfig = getK8sConfig();
+  let { namespace, serviceName, appName, infraRepo } = k8sConfig;
+
+  if (!namespace) {
+    namespace = await prompt("Enter Kubernetes namespace: ");
+  }
+  if (!serviceName) {
+    serviceName = await prompt("Enter Kubernetes service name: ");
+  }
+  if (!appName) {
+    appName = await prompt("Enter Kubernetes app name: ");
+  }
+  if (!infraRepo) {
+    infraRepo = await prompt("Enter Infra Repo: ");
+  }
+
+  // Update package.json with new values
+  updateK8sConfig(namespace, serviceName, appName, infraRepo);
+
+  // Ingress JSON content
+  const ingressJsonContent = JSON.stringify(
+    {
+      apiVersion: "networking.k8s.io/v1",
+      kind: "Ingress",
+      metadata: {
+        name: "ingress-nginx-controller",
+        namespace: namespace,
+      },
+      spec: {
+        ingressClassName: "nginx",
+        rules: [
+          {
+            host: "nuxt-basic.fantasyflip.de",
+            http: {
+              paths: [
+                {
+                  path: "/",
+                  pathType: "Prefix",
+                  backend: {
+                    service: {
+                      name: serviceName,
+                      port: {
+                        number: 3000,
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+    null,
+    2,
+  );
+
+  // Deployment JSON content
+  const deploymentJsonContent = JSON.stringify(
+    {
+      apiVersion: "apps/v1",
+      kind: "Deployment",
+      metadata: {
+        name: appName,
+        namespace: namespace,
+      },
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: {
+            app: appName,
+          },
+        },
+        template: {
+          metadata: {
+            labels: {
+              app: appName,
+            },
+          },
+          spec: {
+            imagePullSecrets: [
+              {
+                name: "ghcr-secret",
+              },
+            ],
+            containers: [
+              {
+                name: appName,
+                image: "ghcr.io/user/repo:branch-hash",
+                env: [],
+                ports: [
+                  {
+                    containerPort: 3000,
+                  },
+                ],
+                resources: {
+                  requests: {
+                    cpu: "500m",
+                    memory: "750Mi",
+                  },
+                  limits: {
+                    cpu: "1000m",
+                    memory: "1000Mi",
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+    null,
+    2,
+  );
+
+  // Service YAML content
+  const serviceYamlContent = `apiVersion: v1
+kind: Service
+metadata:
+  name: ${serviceName}
+  namespace: ${namespace}
+spec:
+  type: ClusterIP
+  ports:
+    - port: 3000
+      protocol: TCP
+      targetPort: 3000
+  selector:
+    app: ${appName}
+`;
+
+  // GitHub Action workflow content
+  const githubActionContent = `name: Dockerize and Push to Infra-Repo
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Use Dockerize and Push to Infra-Repo Action
+        uses: fantasyflip/k8s-build-transfer@v1.1.0
+        with:
+          node-version: 18
+          infra-repo: "${infraRepo}"
+          gh-token: \${{ secrets.GH_TOKEN }}
+          namespace: "${namespace}"
+          app-name: "${appName}"
+`;
+
+  const rootDir = path.join(__dirname, "..");
+  const deploymentDir = path.join(rootDir, "deployment");
+  const podDir = path.join(deploymentDir, "pod");
+  const githubDir = path.join(rootDir, ".github", "workflows");
+
+  // Create directories
+  createDirectory(deploymentDir);
+  createDirectory(podDir);
+  createDirectory(githubDir);
+
+  // Write files
+  fs.writeFileSync(
+    path.join(deploymentDir, "ingress.json"),
+    ingressJsonContent,
+  );
+  fs.writeFileSync(path.join(podDir, "deployment.json"), deploymentJsonContent);
+  fs.writeFileSync(path.join(podDir, "service.yaml"), serviceYamlContent);
+  fs.writeFileSync(
+    path.join(githubDir, "dockerize-and-push.yml"),
+    githubActionContent,
+  );
+
+  console.log("Files created successfully");
+}
+
+// Run the main function
+main();
---
+++ scripts/k8s-sealcreate-secrets.js
@@ -0,0 +1,163 @@
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+const dotenv = require("dotenv");
+const yaml = require("yaml");
+const readline = require("readline");
+
+// Create readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Function to prompt the user for input
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+// Function to encode a string in base64
+const base64Encode = (str) => Buffer.from(str).toString("base64");
+
+// Function to read and update package.json
+const updatePackageJson = (key, value) => {
+  const packageJsonPath = path.resolve(__dirname, "../package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+  if (!packageJson.k8s) {
+    packageJson.k8s = {};
+  }
+  packageJson.k8s[key] = value;
+
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(packageJson, null, 2),
+    "utf8",
+  );
+};
+
+// Function to check if a directory exists
+const checkDirectoryExists = (dirPath) => {
+  return fs.existsSync(dirPath) && fs.lstatSync(dirPath).isDirectory();
+};
+
+// Function to update deployment.json
+const updateDeploymentJson = (envConfig, secretName) => {
+  const deploymentJsonPath = path.resolve(
+    __dirname,
+    "../deployment/pod/deployment.json",
+  );
+  const deploymentJson = JSON.parse(
+    fs.readFileSync(deploymentJsonPath, "utf8"),
+  );
+
+  const envVars = Object.keys(envConfig).map((key) => ({
+    name: key,
+    valueFrom: {
+      secretKeyRef: {
+        key: key,
+        name: secretName,
+      },
+    },
+  }));
+
+  deploymentJson.spec.template.spec.containers[0].env = envVars;
+
+  fs.writeFileSync(
+    deploymentJsonPath,
+    JSON.stringify(deploymentJson, null, 2),
+    "utf8",
+  );
+  console.log(`Updated deployment.json with environment variables`);
+};
+
+// Main function
+const main = async () => {
+  // Check if the deployment directory exists
+  const deploymentDir = path.resolve(__dirname, "../deployment/pod");
+  if (!checkDirectoryExists(deploymentDir)) {
+    console.log(
+      "Deployment directory does not exist. Please run 'pnpm k8s-init'.",
+    );
+    process.exit(1);
+  }
+
+  const packageJsonPath = path.resolve(__dirname, "../package.json");
+  let packageJson;
+
+  try {
+    packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  } catch (error) {
+    console.error("Error reading package.json:", error.message);
+    process.exit(1);
+  }
+
+  let namespace = packageJson.k8s?.namespace;
+  let secretName = packageJson.k8s?.secretName;
+
+  if (!namespace) {
+    namespace = await prompt("Enter the namespace: ");
+    updatePackageJson("namespace", namespace);
+  }
+
+  if (!secretName) {
+    secretName = await prompt("Enter the secret name: ");
+    updatePackageJson("secretName", secretName);
+  }
+
+  // Load environment variables from .env file
+  const envFilePath = path.resolve(__dirname, "../.env");
+  const envConfig = dotenv.parse(fs.readFileSync(envFilePath));
+
+  const outputFilePath = path.resolve(__dirname, `../${secretName}.yaml`);
+  const sealedOutputFilePath = path.resolve(deploymentDir, "env.yaml");
+
+  // Create the secret data object
+  const secretData = {};
+  for (const [key, value] of Object.entries(envConfig)) {
+    secretData[key] = base64Encode(value);
+  }
+
+  // Create the Kubernetes secret manifest
+  const k8sSecret = {
+    apiVersion: "v1",
+    kind: "Secret",
+    metadata: {
+      name: secretName,
+      namespace: namespace,
+    },
+    type: "Opaque",
+    data: secretData,
+  };
+
+  // Write the secret manifest to a YAML file
+  fs.writeFileSync(outputFilePath, yaml.stringify(k8sSecret), "utf8");
+  console.log(`Kubernetes secret manifest created at ${outputFilePath}`);
+
+  // Update deployment.json with environment variables
+  updateDeploymentJson(envConfig, secretName);
+
+  // Run kubeseal on the YAML file
+  exec(
+    `kubeseal --format=yaml < ${outputFilePath} > ${sealedOutputFilePath}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error running kubeseal: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`kubeseal stderr: ${stderr}`);
+        return;
+      }
+      console.log(`Sealed secret created at ${sealedOutputFilePath}`);
+
+      // Delete the unsealed secret YAML file
+      fs.unlinkSync(outputFilePath);
+      console.log(`Unsealed secret YAML file ${outputFilePath} deleted`);
+    },
+  );
+
+  // Close the readline interface
+  rl.close();
+};
+
+// Run the main function
+main();
