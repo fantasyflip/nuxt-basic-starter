4a3391840f85bf5894a1b48fe637258a39d4effc
FantasyFlip <>
2025-07-17 18:36:14 +0200

    chore: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,37 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  // NUXT-OG-IMAGE: .env -> NUXT_PUBLIC_SITE_URL
-  // NUXT-i18n: .env -> NUXT_PUBLIC_I18N_BASE_URL
   modules: [
     "@nuxtjs/tailwindcss",
-    [
-      "@nuxtjs/i18n",
-      {
-        strategy: "prefix_except_default",
-        defaultLocale: "de",
-        locales: [
-          {
-            code: "en",
-            name: "English",
-            language: "en-US",
-            file: "en-US.json",
-            isCatchallLocale: true,
-          },
-          {
-            code: "de",
-            name: "Deutsch",
-            language: "de-DE",
-            file: "de-DE.json",
-          },
-        ],
-        langDir: "locales",
-        lazy: true,
-        vueI18n: "./i18n.config.ts",
-      },
-    ],
+    "@nuxtjs/i18n",
     "nuxt-og-image",
-    [
-      "@nuxtjs/color-mode",
-      {
-        preference: "system", // default value of $colorMode.preference
-        fallback: "dark", // fallback value if not system preference found
-        classSuffix: "",
-        classPrefix: "",
-        storageKey: "theme",
-      },
-    ],
+    "@nuxtjs/color-mode",
     "@vueuse/nuxt",
     "@nuxt/eslint",
   ],
+
+  i18n: {
+    strategy: "prefix_except_default",
+    defaultLocale: "de",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+        language: "en-US",
+        file: "en-US.json",
+        isCatchallLocale: true,
+      },
+      {
+        code: "de",
+        name: "Deutsch",
+        language: "de-DE",
+        file: "de-DE.json",
+      },
+    ],
+    langDir: "locales",
+    vueI18n: "./i18n.config.ts",
+  },
+
   devtools: { enabled: true },
 
   runtimeConfig: {
